c2475f9497190f4d9d60d31eefeba2c9f0016b01
kaorun <hotarunichijou@ik.me>
2026-03-28 21:14:05 +0300

    refactor: floating toolbar state handling

--- app/src/main/java/org/kaorun/nouto/ui/components/TextStyleFloatingToolbar.kt
+++ app/src/main/java/org/kaorun/nouto/ui/components/TextStyleFloatingToolbar.kt
@@ -1,15 +1,17 @@
 package org.kaorun.nouto.ui.components
 
 import android.graphics.Typeface
-import android.text.style.StyleSpan
-import android.text.style.UnderlineSpan
-import android.view.textclassifier.SelectionEvent
-import android.view.textclassifier.TextClassifier
+import android.view.View
+import android.view.View.AccessibilityDelegate
+import android.view.accessibility.AccessibilityEvent
 import com.google.android.material.button.MaterialButton
 import com.onegravity.rteditor.RTEditText
 import com.onegravity.rteditor.RTManager
 import com.onegravity.rteditor.effects.Effect
 import com.onegravity.rteditor.effects.Effects
+import com.onegravity.rteditor.spans.BoldSpan
+import com.onegravity.rteditor.spans.ItalicSpan
+import com.onegravity.rteditor.spans.UnderlineSpan
 import com.onegravity.rteditor.spans.RTSpan
 
 class TextStyleFloatingToolbar(
@@ -19,20 +21,10 @@ class TextStyleFloatingToolbar(
     private val styleUnderline: MaterialButton,
     private val fields: List<RTEditText>
 ) {
-    private var isBold = false
-    private var isItalic = false
-    private var isUnderline = false
-
     fun setupFloatingToolbar() {
-        setupStyleButton(styleBold, Effects.BOLD, Typeface.BOLD) {
-            isBold = it
-        }
-        setupStyleButton(styleItalic, Effects.ITALIC, Typeface.ITALIC) {
-            isItalic = it
-        }
-        setupStyleButton(styleUnderline, Effects.UNDERLINE, null) {
-            isUnderline = it
-        }
+        setupStyleButton(styleBold, Effects.BOLD, Typeface.BOLD)
+        setupStyleButton(styleItalic, Effects.ITALIC, Typeface.ITALIC)
+        setupStyleButton(styleUnderline, Effects.UNDERLINE, null)
         fields.forEach { setupTextClassifier(it) }
     }
 
@@ -40,28 +32,27 @@ class TextStyleFloatingToolbar(
         button: MaterialButton,
         effect: Effect<Boolean, out RTSpan<Boolean>>,
         typeface: Int?,
-        flagUpdater: (Boolean) -> Unit
     ) {
         button.setOnClickListener {
-            val active = when (typeface) {
-                Typeface.BOLD -> !isBold
-                Typeface.ITALIC -> !isItalic
-                null -> !isUnderline
+            val isActive = when (typeface) {
+                Typeface.BOLD -> styleBold.isChecked
+                Typeface.ITALIC -> styleItalic.isChecked
+                null -> styleUnderline.isChecked
                 else -> false
             }
-            flagUpdater(active)
-            button.isChecked = active
-            rtManager.onEffectSelected(effect, active)
+            button.isChecked = isActive
+            rtManager.onEffectSelected(effect, isActive)
         }
     }
 
     private fun setupTextClassifier(textbox: RTEditText) {
-        textbox.setTextClassifier(
-            object : TextClassifier {
-            override fun onSelectionEvent(event: SelectionEvent) = updateButtonsForSelection(textbox)
-        })
-        textbox.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) updateButtonsForSelection(textbox)
+        textbox.accessibilityDelegate = object : AccessibilityDelegate() {
+            override fun sendAccessibilityEvent(host: View, eventType: Int) {
+                super.sendAccessibilityEvent(host, eventType)
+                if (eventType == AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED) {
+                    updateButtonsForSelection(textbox)
+                }
+            }
         }
     }
 
@@ -70,18 +61,8 @@ class TextStyleFloatingToolbar(
         val start = textbox.selectionStart
         val end = textbox.selectionEnd
 
-        isBold = spannable.getSpans(start, end, StyleSpan::class.java).any {
-            it.style == Typeface.BOLD
-        }
-        isItalic = spannable.getSpans(start, end, StyleSpan::class.java).any {
-            it.style == Typeface.ITALIC
-        }
-        isUnderline = spannable.getSpans(start, end, UnderlineSpan::class.java).any {
-            spannable.getSpanStart(it) <= end && spannable.getSpanEnd(it) >= start
-        }
-
-        styleBold.isChecked = isBold
-        styleItalic.isChecked = isItalic
-        styleUnderline.isChecked = isUnderline
+        styleBold.isChecked = spannable.getSpans(start, end, BoldSpan::class.java).isNotEmpty()
+        styleItalic.isChecked = spannable.getSpans(start, end, ItalicSpan::class.java).isNotEmpty()
+        styleUnderline.isChecked = spannable.getSpans(start, end, UnderlineSpan::class.java).isNotEmpty()
     }
 }
