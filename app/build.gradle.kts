9b0e6937b8367fff51a58513295e4120d3a67935
kaorun <hotarunichijou@ik.me>
2026-05-01 09:11:59 +0300

    build: prepare for 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "org.kaorun.nouto"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
